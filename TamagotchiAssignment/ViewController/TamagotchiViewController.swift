93f25997bd56dc0c8d84ccbcc446224ae83bb1c4
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-08 23:42:00 +0900

    뷰컨트롤러 폴더 이동 및 셀 구성 코드 추가

---
+++ TamagotchiAssignment/ViewController/TamagotchiViewController.swift
@@ -0,0 +1,86 @@
+//
+//  TamagotchiViewController.swift
+//  TamagotchiAssignment
+//
+//  Created by Minjae Kim on 6/7/24.
+//
+
+import UIKit
+import SnapKit
+
+final class TamagotchiViewController: UIViewController, ConfigureViewProtocol {
+    
+    let tamagotchiCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
+    private let userDefaultsHelper = UserDefaultsHelper.shared
+    
+    private var tamagotchies: [Tamagotchi] = []
+    
+    public var navigationTitle = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 245/255, green: 252/255, blue: 252/255, alpha: 1)
+        
+        navigationItem.title = navigationTitle
+        
+        tamagotchies = userDefaultsHelper.getTamagochi()
+        
+        configureHierarchy()
+        configureLayout()
+        configureCollectionView()
+    }
+    
+    func configureHierarchy() {
+        view.addSubview(tamagotchiCollectionView)
+    }
+    
+    func configureLayout() {
+        tamagotchiCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.bottom.equalToSuperview()
+        }
+    }
+    
+    private func configureCollectionView() {
+        tamagotchiCollectionView.delegate = self
+        tamagotchiCollectionView.dataSource = self
+        tamagotchiCollectionView.register(TamagotchiCollectionViewCell.self, forCellWithReuseIdentifier: TamagotchiCollectionViewCell.identifier)
+        tamagotchiCollectionView.backgroundColor = .clear
+        tamagotchiCollectionView.showsVerticalScrollIndicator = false
+    }
+}
+
+extension TamagotchiViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let margin: CGFloat = 16
+        let width: CGFloat = (collectionView.bounds.width - (margin * 6)) / 3
+        let height: CGFloat = width * 1.4
+        
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        print(#function, indexPath.row)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(#function, indexPath.row)
+    }
+}
+
+extension TamagotchiViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return tamagotchies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TamagotchiCollectionViewCell.identifier, for: indexPath) as? TamagotchiCollectionViewCell else { return UICollectionViewCell() }
+        let index = indexPath.row
+        cell.tamagotchi = tamagotchies[index]
+        cell.configureContent()
+        return cell
+    }
+}
