73175e92f0ce7b36001d9effd7505b8090f89e9d
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-09 01:21:46 +0900

    자주 사용하는 UIColor extension에 추가

---
+++ TamagotchiAssignment/Extension/UIColor+.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+.swift
+//  TamagotchiAssignment
+//
+//  Created by Minjae Kim on 6/8/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static let tamagotchiBackgroundColor = UIColor(red: 245/255, green: 252/255, blue: 252/255, alpha: 1)
+    static let tamagotchiBorderColor = UIColor(red: 77/255, green: 106/255, blue: 120/255, alpha: 1)
+}
