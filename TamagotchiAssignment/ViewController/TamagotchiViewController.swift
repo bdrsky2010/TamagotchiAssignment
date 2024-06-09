9ce365d4428abf804ac0938c68016e26d0a87fd5
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-09 23:38:39 +0900

    변경사항 많음
    - 키보드 올라오는 것을 감지해서 올려주기 위해 스크롤뷰로 변경
    - 키보드 올라오고 내려가는 것을 NotificationCenter에서 감지하여 키보드의 높이만큼 스크롤을 올려줌
    
    - 스크롤을 드래그하면 키보드 내려가는 방식이 아닌 드래그 하면서 키보드도 같이 내려야 키보드가 내려가는 방식으로 변경
    
    - 오른쪽 네비게이션 버튼 아이템 추가하여 세팅뷰를 네비게이션에 푸시
    
    - textField.inputAccessoryView에 UIToolBar 를 추가하여 키보드 위에 툴바를 추가하였음. 툴바에 있는 Done 버튼을 누르면 키보드가 내려감
    
    - 밥먹기, 물먹기 버튼 클릭 시, 다마고치의 구조체 데이터에 값을 저장해주면서 setter 메서드를 통해 바로 UserDefaults에 저장하고 다시 불러와서 뷰를 다시 그려줌

--- TamagotchiAssignment/ViewController/TamagotchiViewController.swift
+++ TamagotchiAssignment/ViewController/TamagotchiViewController.swift
@@ -6,21 +6,37 @@
 //
 
 import UIKit
+import SnapKit
 
-class TamagotchiViewController: UIViewController, ConfigureViewProtocol {
+final class TamagotchiViewController: UIViewController, ConfigureViewProtocol {
 
+    let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.showsVerticalScrollIndicator = true
+        scrollView.keyboardDismissMode = .interactive
+        return scrollView
+    }()
+    
+    let contentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
+    }()
+    
     let randomBubbleImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "bubble")
-        imageView.contentMode = .scaleAspectFit
+        imageView.contentMode = .scaleToFill
         return imageView
     }()
     
     let randomBubbleLabel: UILabel = {
         let label = UILabel()
-        label.font = .systemFont(ofSize: 11, weight: .bold)
+        label.text = "토할거가타요ㅠㅁㅠ"
+        label.font = .systemFont(ofSize: 13, weight: .bold)
         label.textColor = UIColor.tamagotchiBorderColor
         label.textAlignment = .center
+        label.numberOfLines = 0
         return label
     }()
     
@@ -44,8 +60,109 @@ class TamagotchiViewController: UIViewController, ConfigureViewProtocol {
         label.text = "준비중이에요"
         label.font = .systemFont(ofSize: 14, weight: .bold)
         label.textColor = UIColor.tamagotchiBorderColor
-        
         return label
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "LV1 ∙ 밥알 0개 ∙ 물방울 0개"
+        label.font = .systemFont(ofSize: 14, weight: .bold)
+        label.textColor = UIColor.tamagotchiBorderColor
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let eatTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .none
+        textField.attributedPlaceholder = NSAttributedString(
+            string: "밥주세용", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14, weight: .bold)])
+        textField.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        textField.textAlignment = .center
+        textField.textColor = UIColor.tamagotchiBorderColor
+        textField.keyboardType = .numberPad
+        return textField
+    }()
+    
+    let eatUnderBar: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.tamagotchiBorderColor
+        return view
+    }()
+    
+    let eatButton: UIButton = {
+        let button = UIButton()
+        button.configuration = .plain()
+        button.configuration?.baseBackgroundColor = UIColor.clear
+        button.layer.borderColor = UIColor.tamagotchiBorderColor.cgColor
+        button.layer.borderWidth = 1.5
+        button.layer.cornerRadius = 5
+        button.configuration?.image = UIImage(systemName: "leaf.circle")
+        
+        button.configuration?.preferredSymbolConfigurationForImage = .init(pointSize: 14)
+        button.configuration?.imagePadding = 4
+        button.configuration?.contentInsets = .init(top: 0, leading: -16, bottom: 0, trailing: -16)
+        
+        button.configurationUpdateHandler = { btn in
+            switch btn.state {
+            case .highlighted:
+                btn.tintColor = UIColor.tamagotchilightBackgroundColor
+                btn.configuration?.attributedTitle = AttributedString(
+                    NSAttributedString(string: "밥먹기", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13, weight: .bold), NSAttributedString.Key.foregroundColor: UIColor.tamagotchilightBackgroundColor]))
+            default:
+                btn.tintColor = UIColor.tamagotchiBorderColor
+                btn.configuration?.attributedTitle = AttributedString(
+                    NSAttributedString(string: "밥먹기", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13, weight: .bold), NSAttributedString.Key.foregroundColor: UIColor.tamagotchiBorderColor]))
+            }
+        }
+        return button
+    }()
+    
+    let drinkTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .none
+        textField.attributedPlaceholder = NSAttributedString(
+            string: "물주세용", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14, weight: .bold)])
+        textField.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        textField.textColor = UIColor.tamagotchiBorderColor
+        textField.textAlignment = .center
+        textField.textColor = UIColor.tamagotchiBorderColor
+        textField.keyboardType = .numberPad
+        return textField
+    }()
+    
+    let drinkUnderBar: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.tamagotchiBorderColor
+        return view
+    }()
+    
+    let drinkButton: UIButton = {
+        let button = UIButton()
+        button.configuration = .plain()
+        button.configuration?.baseBackgroundColor = UIColor.clear
+        button.layer.borderColor = UIColor.tamagotchiBorderColor.cgColor
+        button.layer.borderWidth = 1.5
+        button.layer.cornerRadius = 5
+        button.configuration?.image = UIImage(systemName: "leaf.circle")
+        
+        button.configuration?.preferredSymbolConfigurationForImage = .init(pointSize: 14)
+        button.configuration?.imagePadding = 4
+        button.configuration?.contentInsets = .init(top: 0, leading: -16, bottom: 0, trailing: -16)
+        
+        button.configurationUpdateHandler = { btn in
+            switch btn.state {
+            case .highlighted:
+                btn.tintColor = UIColor.tamagotchilightBackgroundColor
+                btn.configuration?.attributedTitle = AttributedString(
+                    NSAttributedString(string: "물먹기", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13, weight: .bold), NSAttributedString.Key.foregroundColor: UIColor.tamagotchilightBackgroundColor]))
+            default:
+                btn.tintColor = UIColor.tamagotchiBorderColor
+                btn.configuration?.attributedTitle = AttributedString(
+                    NSAttributedString(string: "물먹기", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13, weight: .bold), NSAttributedString.Key.foregroundColor: UIColor.tamagotchiBorderColor]))
+            }
+        }
+        return button
     }()
     
     private let userDefaultsHelper = UserDefaultsHelper.shared
@@ -55,8 +172,26 @@ class TamagotchiViewController: UIViewController, ConfigureViewProtocol {
     }
     
     private var tamagotchi: Tamagotchi? {
-        return userDefaultsHelper.getSelectTamagotchi()
+        get {
+            return userDefaultsHelper.getSelectTamagotchi()
+        }
+        set {
+            guard let newValue else { return }
+            userDefaultsHelper.setSelectTamagochi(newValue)
+            configureContent()
+        }
     }
+    
+    lazy var randomBubble: [String] = [
+        "\(nickname)님 오늘 깃허브 푸시 하셨어영?",
+        "\(nickname)님 오늘 과제 하셨어용?",
+        "콜렉션 뷰 어렵당 ㅠㅠ",
+        "\(nickname)님 토할거가타요ㅠㅁㅠ",
+        "목말라용 ~~~~~~",
+        "취업 언제 하냐고오오오",
+        "오토레이아웃 너무 어렵지 않나요???",
+        "\(nickname)님 저 너무 배가 고파욤 ㅠㅁㅠ"
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -67,11 +202,48 @@ class TamagotchiViewController: UIViewController, ConfigureViewProtocol {
         configureHierarchy()
         configureLayout()
         configureContent()
+        
+        configureNotificationCenter()
+        configureTextField()
+        confgureButtonAction()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationItem.title = nickname + "님의 다마고치"
+    }
+    
+    private func configureNotificationCenter() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
+            return
+        }
+        
+        scrollView.contentInset.bottom = keyboardFrame.size.height
+        
+        
+    }
+    
+    @objc private func keyboardWillHide() {
+        let contentInset = UIEdgeInsets.zero
+        scrollView.contentInset = contentInset
+        scrollView.scrollIndicatorInsets = contentInset
     }
     
     func configureNavigation() {
-        navigationItem.title = nickname + "님의 다마고치"
-        
         let rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.crop.circle"),
                                                  style: .plain, target: self, action: #selector(rightBarButtonItemClicked))
         
@@ -81,17 +253,193 @@ class TamagotchiViewController: UIViewController, ConfigureViewProtocol {
     @objc
     private func rightBarButtonItemClicked() {
         print(#function)
+        
+        let settingViewController = SettingViewController()
+        navigationController?.pushViewController(settingViewController, animated: true)
     }
     
     func configureHierarchy() {
         
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        
+        contentView.addSubview(tamagotchiImageView)
+        contentView.addSubview(randomBubbleImageView)
+        contentView.addSubview(randomBubbleLabel)
+        
+        nameBackgroundView.addSubview(nameLabel)
+        contentView.addSubview(nameBackgroundView)
+        contentView.addSubview(descriptionLabel)
+        contentView.addSubview(eatTextField)
+        contentView.addSubview(eatUnderBar)
+        contentView.addSubview(eatButton)
+        contentView.addSubview(drinkTextField)
+        contentView.addSubview(drinkUnderBar)
+        contentView.addSubview(drinkButton)
     }
     
     func configureLayout() {
         
+        scrollView.snp.makeConstraints { make in
+            make.horizontalEdges.verticalEdges.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        contentView.snp.makeConstraints { make in
+            make.edges.equalTo(scrollView.contentLayoutGuide)
+            make.width.equalTo(scrollView.frameLayoutGuide)
+            make.height.equalTo(scrollView.frameLayoutGuide)
+        }
+        
+        tamagotchiImageView.snp.makeConstraints { make in
+            make.width.equalToSuperview().multipliedBy(0.5)
+            make.height.equalToSuperview().multipliedBy(0.25)
+            make.centerX.equalToSuperview()
+        }
+        
+        randomBubbleImageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(40)
+            make.centerX.equalTo(tamagotchiImageView)
+            make.width.equalTo(tamagotchiImageView.snp.width).multipliedBy(1.2)
+            make.height.equalToSuperview().multipliedBy(0.2)
+            make.bottom.equalTo(tamagotchiImageView.snp.top).offset(-4)
+        }
+        
+        randomBubbleLabel.snp.makeConstraints { make in
+            make.top.equalTo(randomBubbleImageView.snp.top).offset(8)
+            make.centerX.equalTo(randomBubbleImageView.snp.centerX)
+            make.horizontalEdges.equalTo(randomBubbleImageView.snp.horizontalEdges).inset(8)
+            make.bottom.equalTo(randomBubbleImageView.snp.bottom).offset(-20)
+        }
+        
+        nameBackgroundView.snp.makeConstraints { make in
+            make.top.equalTo(tamagotchiImageView.snp.bottom).offset(8)
+            make.centerX.equalTo(tamagotchiImageView.snp.centerX)
+        }
+        
+        nameLabel.snp.makeConstraints { make in
+            make.horizontalEdges.verticalEdges.equalToSuperview().inset(6)
+            make.height.equalTo(20)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(nameLabel.snp.bottom).offset(16)
+            make.centerX.equalTo(nameLabel.snp.centerX)
+        }
+        
+        eatButton.snp.makeConstraints { make in
+            make.top.equalTo(descriptionLabel.snp.bottom).offset(32)
+            make.trailing.equalTo(contentView.safeAreaLayoutGuide).offset(-80)
+            make.height.equalTo(35)
+            make.width.equalTo(scrollView.snp.width).multipliedBy(0.18)
+        }
+        
+        eatTextField.snp.makeConstraints { make in
+            make.leading.equalTo(contentView.safeAreaLayoutGuide).offset(80)
+            make.centerY.equalTo(eatButton.snp.centerY)
+            make.trailing.equalTo(eatButton.snp.leading).offset(-12)
+        }
+        
+        eatUnderBar.snp.makeConstraints { make in
+            make.leading.equalTo(eatTextField.snp.leading)
+            make.trailing.equalTo(eatTextField.snp.trailing)
+            make.height.equalTo(1.5)
+            make.bottom.equalTo(eatButton.snp.bottom)
+        }
+        
+        drinkButton.snp.makeConstraints { make in
+            make.top.equalTo(eatButton.snp.bottom).offset(12)
+            make.trailing.equalTo(contentView).offset(-80)
+            make.height.equalTo(35)
+            make.width.equalTo(contentView.snp.width).multipliedBy(0.18)
+        }
+        
+        drinkTextField.snp.makeConstraints { make in
+            make.leading.equalTo(contentView).offset(80)
+            make.centerY.equalTo(drinkButton.snp.centerY)
+            make.trailing.equalTo(drinkButton.snp.leading).offset(-12)
+        }
+        
+        drinkUnderBar.snp.makeConstraints { make in
+            make.leading.equalTo(drinkTextField.snp.leading)
+            make.trailing.equalTo(drinkTextField.snp.trailing)
+            make.height.equalTo(1.5)
+            make.bottom.equalTo(drinkButton.snp.bottom)
+        }
     }
     
     func configureContent() {
+        guard let tamagotchi else { return }
         
+        randomBubbleLabel.text = randomBubble.randomElement()
+        tamagotchiImageView.image = UIImage(named: "\(tamagotchi.id)-\(tamagotchi.level == 10 ? 9 : tamagotchi.level)")
+        nameLabel.text = tamagotchi.name
+        descriptionLabel.text = "LV\(tamagotchi.level) ∙ 밥알 \(tamagotchi.rice)개 ∙ 물방울 \(tamagotchi.water)개"
+    }
+    
+    private func confgureButtonAction() {
+        eatButton.addTarget(self, action: #selector(eatButtonClicked), for: .touchUpInside)
+        drinkButton.addTarget(self, action: #selector(drinkButtonClicked), for: .touchUpInside)
+    }
+    
+    @objc
+    private func eatButtonClicked() {
+        guard let tamagotchi, let text = eatTextField.text else { return }
+        
+        if text.count == 0 {
+            self.tamagotchi = tamagotchi.addRice(1)
+            eatTextField.text = nil
+        }
+        
+        if let eatCount = Int(text), eatCount < 100 {
+            self.tamagotchi = tamagotchi.addRice(eatCount)
+            eatTextField.text = nil
+        }
+    }
+    
+    @objc
+    private func drinkButtonClicked() {
+        guard let tamagotchi, let text = drinkTextField.text else { return }
+        
+        if text.count == 0 {
+            self.tamagotchi = tamagotchi.addWater(1)
+            drinkTextField.text = nil
+        }
+        
+        if let drinkCount = Int(text), drinkCount < 50 {
+            self.tamagotchi = tamagotchi.addWater(drinkCount)
+            drinkTextField.text = nil
+        }
+    }
+}
+
+extension TamagotchiViewController: UITextFieldDelegate {
+    
+    private func configureTextField() {
+        eatTextField.delegate = self
+        drinkTextField.delegate = self
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        let toolBar = UIToolbar(frame: CGRect(x: 0.0,
+                                              y: 0.0,
+                                              width: UIScreen.main.bounds.size.width,
+                                              height: 44.0))
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let DoneButton = UIBarButtonItem(barButtonSystemItem: .done, target: target, action: #selector(doneButtonClicked))
+        let titleItem = UIBarButtonItem(title: textField.placeholder, style: .plain, target: nil, action: nil)
+        titleItem.tintColor = UIColor.systemGray
+        toolBar.setItems([flexibleSpace, titleItem, flexibleSpace, DoneButton], animated: false)
+        textField.inputAccessoryView = toolBar
+    }
+    
+    @objc
+    private func doneButtonClicked() {
+        if eatTextField.isFirstResponder {
+            eatTextField.resignFirstResponder()
+        }
+        
+        if drinkTextField.isFirstResponder {
+            drinkTextField.resignFirstResponder()
+        }
     }
 }
