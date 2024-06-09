731bb71dd95e0c2bf0d30fdffd7cbb1073b04733
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-09 23:23:30 +0900

    쉽게 id 값을 받기 위한 extension

---
+++ TamagotchiAssignment/Extension/UICollectionViewCell+.swift
@@ -0,0 +1,14 @@
+//
+//  UICollectionViewCell+.swift
+//  TamagotchiAssignment
+//
+//  Created by Minjae Kim on 6/9/24.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
