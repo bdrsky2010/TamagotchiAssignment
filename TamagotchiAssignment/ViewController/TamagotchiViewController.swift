12e9f8c82a97c296cc25a9d4a9a2887450ce2f78
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-09 16:52:16 +0900

    다마고치의 메인 뷰컨트롤러 뷰 그리는중

--- TamagotchiAssignment/ViewController/TamagotchiViewController.swift
+++ TamagotchiAssignment/ViewController/TamagotchiViewController.swift
@@ -2,98 +2,96 @@
 //  TamagotchiViewController.swift
 //  TamagotchiAssignment
 //
-//  Created by Minjae Kim on 6/7/24.
+//  Created by Minjae Kim on 6/9/24.
 //
 
 import UIKit
-import SnapKit
 
-final class TamagotchiViewController: UIViewController, ConfigureViewProtocol {
+class TamagotchiViewController: UIViewController, ConfigureViewProtocol {
+
+    let randomBubbleImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "bubble")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
     
-    let tamagotchiCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    let randomBubbleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 11, weight: .bold)
+        label.textColor = UIColor.tamagotchiBorderColor
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let tamagotchiImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "noImage")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    let nameBackgroundView: UIView = {
+        let view = UIView()
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.tamagotchiBorderColor.cgColor
+        view.layer.cornerRadius = 5
+        return view
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "준비중이에요"
+        label.font = .systemFont(ofSize: 14, weight: .bold)
+        label.textColor = UIColor.tamagotchiBorderColor
+        
+        return label
+    }()
     
     private let userDefaultsHelper = UserDefaultsHelper.shared
     
-    private var tamagotchies: [Tamagotchi] = []
+    private var nickname: String {
+        return userDefaultsHelper.getNickname()
+    }
     
-    public var navigationTitle = "" {
-        didSet {
-            navigationItem.title = navigationTitle
-        }
+    private var tamagotchi: Tamagotchi {
+        return userDefaultsHelper.getSelectTamagotchi()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         view.backgroundColor = UIColor.tamagotchiBackgroundColor
-        print(#function)
         
-        tamagotchies = userDefaultsHelper.getTamagochi()
-        
+        configureNavigation()
         configureHierarchy()
         configureLayout()
-        configureCollectionView()
+        configureContent()
+    }
+    
+    func configureNavigation() {
+        navigationItem.title = nickname + "님의 다마고치"
+        
+        let rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.crop.circle"),
+                                                 style: .plain, target: self, action: #selector(rightBarButtonItemClicked))
+        
+        navigationItem.rightBarButtonItem = rightBarButtonItem
+    }
+    
+    @objc
+    private func rightBarButtonItemClicked() {
+        print(#function)
     }
     
     func configureHierarchy() {
-        view.addSubview(tamagotchiCollectionView)
+        
     }
     
     func configureLayout() {
-        tamagotchiCollectionView.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide)
-            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
-            make.bottom.equalToSuperview()
-        }
+        
     }
     
-    private func configureCollectionView() {
-        tamagotchiCollectionView.delegate = self
-        tamagotchiCollectionView.dataSource = self
-        tamagotchiCollectionView.register(TamagotchiCollectionViewCell.self, forCellWithReuseIdentifier: TamagotchiCollectionViewCell.identifier)
-        tamagotchiCollectionView.backgroundColor = .clear
-        tamagotchiCollectionView.showsVerticalScrollIndicator = false
-    }
-}
-
-extension TamagotchiViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    func configureContent() {
         
-        let margin: CGFloat = 16
-        let width: CGFloat = (collectionView.bounds.width - (margin * 6)) / 3
-        let height: CGFloat = width * 1.4
-        
-        return CGSize(width: width, height: height)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print(#function, indexPath.row)
-        
-        let tamagotchi = tamagotchies[indexPath.row]
-        
-        if tamagotchi.isAvailable {
-            let tamagotchiPopupViewController = TamagotchiPopupViewController()
-            tamagotchiPopupViewController.tamagotchi = tamagotchies[indexPath.row]
-            
-            // MARK: fullScreen vs overFullScreen
-            // 'fullScreen'은 부모뷰를 모달 뷰가 다 가려버리는 modal style
-            // 'overFullScreen'은 fullScreen처럼 모달 뷰가 다 가리긴하지만 배경색이 투명해지며
-            // 부모뷰의 일부분이 보임
-            tamagotchiPopupViewController.modalPresentationStyle = .overFullScreen
-            present(tamagotchiPopupViewController, animated: true)
-        }
-    }
-}
-
-extension TamagotchiViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return tamagotchies.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TamagotchiCollectionViewCell.identifier, for: indexPath) as? TamagotchiCollectionViewCell else { return UICollectionViewCell() }
-        let index = indexPath.row
-        cell.tamagotchi = tamagotchies[index]
-        cell.configureContent()
-        return cell
     }
 }
