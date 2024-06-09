c40ec07c2b3dd88616d98fb7cb29704c6ef5ebd4
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-09 23:23:43 +0900

    쉽게 id 값을 받기 위한 extension

---
+++ TamagotchiAssignment/Extension/UITableViewCell+.swift
@@ -0,0 +1,14 @@
+//
+//  UITableViewCell+.swift
+//  TamagotchiAssignment
+//
+//  Created by Minjae Kim on 6/9/24.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
