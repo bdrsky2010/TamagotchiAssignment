bfd6fb2ec315dad6493715710fc698d08b1865fe
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-09 16:38:53 +0900

    사용하는 컬러 에셋 추가

--- TamagotchiAssignment/Extension/UIColor+.swift
+++ TamagotchiAssignment/Extension/UIColor+.swift
@@ -9,5 +9,7 @@ import UIKit
 
 extension UIColor {
     static let tamagotchiBackgroundColor = UIColor(red: 245/255, green: 252/255, blue: 252/255, alpha: 1)
-    static let tamagotchiBorderColor = UIColor(red: 77/255, green: 106/255, blue: 120/255, alpha: 1)
+    static let tamagotchiButtonBackgroundColor = UIColor(red: 228/255, green: 235/255, blue: 237/255, alpha: 1)
+    static let tamagotchilightBackgroundColor = UIColor(red: 222/255, green: 230/255, blue: 240/255, alpha: 1)
+    static let tamagotchiBorderColor = UIColor(red: 77/255, green: 106/255, blue: 120/255, alpha: 0.8)
 }
